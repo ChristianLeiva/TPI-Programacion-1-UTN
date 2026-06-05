24f901168d438e2cfe4152172b31284504ea851f
SolOrtiz0708 <solortiz1029@gmail.com>
2026-06-05 17:46:55 -0300

    Estructura del código
    Added function definitions and main program loop for CSV handling.

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
-# # Acá empezamos a plantear el código
+import csv
+
+#Definir funciones
+
+def cargar_cvs():
+    pass
+
+def mostrar_menu():
+    pass
+
+def capturar_opcion():
+    pass
+
+def calcular_estadísticas():
+    pass
+
+def ordenar_países():
+    pass
+
+def exportar_csv():
+    pass
+
+#Programa principal
+datos = cargar_cvs()
+
+while True:
+    mostrar_menu()
+    opcion= capturar_opcion()
+    
+    if opcion == 1:
+        calcular_estadísticas()
+    elif opcion == 2:
+        ordenar_países()
+    elif opcion == 3:
+        exportar_csv()
+    elif opcion == 4:
+        print("Saliendo del programa...")
+        break
