6d79c9e516da4adebbeba391ab007a8ba9b74676
SolOrtiz0708 <solortiz1029@gmail.com>
2026-06-10 19:24:28 -0300

    Actualización: avance del código

--- main.py
+++ main.py
@@ -1,38 +1,73 @@
 import csv
 
-#Definir funciones
+#Variable global
+archivo = 'datos.csv'
 
-def cargar_cvs():
-    pass
+#Definir funciones
+def cargar_csv():
+    datos = []
+    with open(archivo, 'r') as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            datos.append(fila)
+    return datos
 
 def mostrar_menu():
+    print("Menú de opciones:")
+    print("1. Agregar nuevo país")
+    print("2. Actualizar datos población/superficie de un país")
+    print("3. Buscar país por nombre")
+    print("4. Filtrar países")
+    print("5. Ordenar países")
+    print("6. Mostrar estadísticas")
+    print("7. Salir")
+
+def agregar_país():
     pass
 
-def capturar_opcion():
+def actualizar_datos():
     pass
 
-def calcular_estadísticas():
+def buscar_país():
+    pass
+
+def filtrar_países():
     pass
 
 def ordenar_países():
     pass
 
-def exportar_csv():
+def mostrar_países():
     pass
+
 
 #Programa principal
 datos = cargar_cvs()
 
 while True:
     mostrar_menu()
-    opcion= capturar_opcion()
-    
-    if opcion == 1:
-        calcular_estadísticas()
-    elif opcion == 2:
-        ordenar_países()
-    elif opcion == 3:
-        exportar_csv()
-    elif opcion == 4:
-        print("Saliendo del programa...")
-        break
+    try:
+        opcion = int(input("Ingrese una opción (1-7): "))
+
+        if opcion not in [1, 2, 3, 4, 5, 6, 7]:
+            print("Opción inválida. Por favor, ingrese un número entre 1 y 7.")
+        
+        else:
+            if opcion == 1:
+                agregar_país()
+            elif opcion == 2:
+                pass
+            elif opcion == 3:
+                pass
+            elif opcion == 4:
+                pass
+            elif opcion == 5:
+                pass
+            elif opcion == 6:
+                pass
+            else:
+                print("Saliendo del programa...")
+                break
+            
+    except ValueError:
+        print("Entrada no válida. Por favor, ingrese un número entre 1 y 7.")
